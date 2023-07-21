9ac0d2df0cbde2dfaf48d56297682b57d57229c8
Andrey <andrei4-09@mail.ru>
2023-07-21 10:38:56 +0300

    Разобрал проект по папкам

---
+++ gateway/src/main/java/ru/practicum/shareit/ItemController.java
@@ -0,0 +1,67 @@
+package ru.practicum.shareit;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.ItemService;
+import ru.practicum.shareit.item.dto.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    private final String USER_ID = "X-Sharer-User-Id";
+
+    @PostMapping
+    public ItemDto addItem(@Valid @RequestBody ItemDto itemDto, @RequestHeader(value = USER_ID) Long userId) {
+        log.debug(itemDto + " - получен запрос на добавление вещи");
+        itemDto.setOwner(userId);
+        return itemService.addItem(itemDto);
+    }
+
+    @PatchMapping(value = "/{itemId}")
+    public ItemDto partialUpdate(
+            @RequestBody ItemUpdateDto itemUpdateDto,
+            @PathVariable("itemId") Long itemId,
+            @RequestHeader(value = USER_ID) Long userId) {
+        log.debug("Вещь с ID = {} был обновлен", itemId);
+        return itemService.updateItem(itemUpdateDto, userId, itemId);
+    }
+
+    @GetMapping(value = "/{itemId}")
+    public ItemPublicDto getItemByIdByUserId(@PathVariable("itemId") Long itemId,
+            @RequestHeader(value = USER_ID) Long userId) {
+        log.debug("Вещь с ID = {} была запрошена", itemId);
+        return itemService.findByIdAndOwner(itemId, userId);
+    }
+
+    @GetMapping
+    public List<ItemPublicDto> getAllByUser(@RequestHeader(value = USER_ID) Long userId) {
+        log.debug("Запрос всех вещей пользователя ID = {}", userId);
+        return itemService.findAllByUser(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> getItemByParam(@RequestParam(value = "text") String text) {
+        log.debug("Получен запрос на поиск вещи с текстом = {} ", text);
+        return itemService.getItemByParam(text);
+    }
+
+    @PostMapping(value = "/{itemId}/comment")
+    public CommentDto addComment(@PathVariable("itemId") Long itemId, @Valid @RequestBody CommentAddDto commentAddDto,
+                                 @RequestHeader(value = USER_ID) Long userId) {
+        log.debug(commentAddDto + " - получен запрос на добавление отзыва");
+        commentAddDto.setUserId(userId);
+        commentAddDto.setItemId(itemId);
+        commentAddDto.setCreated(LocalDateTime.now());
+        return itemService.addComment(commentAddDto);
+    }
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/UserController.java
@@ -0,0 +1,52 @@
+package ru.practicum.shareit;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.UserService;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto addUser(@Valid @RequestBody UserDto userDto) {
+        log.debug(userDto + " - пользователь был добавлен");
+        return userService.addUser(userDto);
+    }
+
+    @PatchMapping(value = "/{userId}")
+    public UserDto updateUser(@PathVariable Long userId, @RequestBody UserUpdateDto userUpdateDto) {
+        userUpdateDto.setId(userId);
+        userService.updateUser(userUpdateDto);
+        log.debug("Получен запрос на обновление пользователя с  ID = {} ", userId);
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping(value = "/{userId}")
+    public UserDto getUserById(@PathVariable Long userId) {
+        log.debug("Получен запрос  пользователя с  ID = {} ", userId);
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        log.debug("Получен запрос всех пользователей");
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping(value = "{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        log.debug("Пользователь был удален");
+    }
+}
